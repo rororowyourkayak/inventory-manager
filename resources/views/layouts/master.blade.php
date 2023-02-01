7de77a8ef54b79ddf54603f313b90ee1eca66021
rororowyourkayak <roshanforde@gmail.com>
2023-02-01 13:39:29 -0500

    small change to bootstrap version with the master layout, tweaks with the logged in navbar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,10 +9,9 @@ use App\Models\Item;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    
 </head>
 @auth
     @include('reusable_snippets/navbar_for_logged_in_pages')
@@ -26,7 +25,10 @@ use App\Models\Item;
         @yield('content')
     </div>
 </body>
-
+<footer class="text-center mt-4">
+    <hr>
+    Inventory Manager - Created by Roshan Forde
+</footer>
 </html>
 
     
--- resources/views/reusable_snippets/navbar_for_logged_in_pages.php
+++ resources/views/reusable_snippets/navbar_for_logged_in_pages.php
@@ -1,9 +1,10 @@
 <nav class="navbar navbar-expand-sm sticky-top navbar-dark" style="background-color: steelblue;">
+<div class="container-fluid">
  <a class="navbar-brand" href="/home">Inventory Manager</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
   <ul class="navbar-nav">
     
     <li class="nav-item">
@@ -16,5 +17,6 @@
       <a class="nav-link" href="/logout">Logout</a>
     </li>
   </ul>
+</div>
 </div>
 </nav>
